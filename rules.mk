5c85dc5c8b21a8971e9da16e0c94ee69aa8c1a15
Yerko Cortés baeza <ykacorb@gmail.com>
2025-06-05 21:42:09 -0400

    El nacimiento

---
+++ rules.mk
@@ -0,0 +1,8 @@
+# OLED_ENABLE = yes        # Habilita el soporte para pantallas OLED.
+# ENCODER_MAP_ENABLE = yes # Activa la configuración de encoders por capa.
+# TRI_LAYER_ENABLE = yes   # Permite usar tri-layer (LOWER + RAISE = ADJUST).
+# RGBLIGHT_ENABLE = no    # Habilitar RGB.
+# # OLED_DRIVER = SSD1306  # Opcional, si usas este driver.
+
+
+TRI_LAYER_ENABLE = yes
